6c36b90e96681e2313cacce2488d10db0eb92175
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-12-23 17:31:53 -0400

    basic setup with functional nav bar

---
+++ src/pages/about.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Layout from "@/app/layout";
+
+const AboutPage: React.FC = () => {
+  return (
+    <section>
+      <h1>About Us</h1>
+      <p>Welcome to the About page of Travis and Paulinas Church.</p>
+      <p>
+        Here, you can find information about our church, our mission, and our
+        community.
+      </p>
+      {/* Add more content as needed */}
+    </section>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/app/layout";
+
+export default function Home() {
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
