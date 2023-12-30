d9827b9f2daf33bd60baec73958b1f3b0c0294c8
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-12-30 17:23:11 -0400

    final touches to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa"; // Import the icons
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
@@ -18,15 +19,19 @@ const Footer: React.FC = () => {
             src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=9.845323,-83.940137`}
           ></iframe>
         </div>
-        {/* <div className="linkSection">
-        </div> */}
         <div className="socialMediaSection">
-          <a href="#!">About Us</a>
-          <a href="#!">Contact</a>
-          <a href="#!">Privacy Policy</a>
-          <a href="#!"><FaFacebookF /></a>
-          <a href="#!"><FaTwitter /></a>
-          <a href="#!"><FaInstagram /></a>
+          <Link href="/about">About Us</Link>
+          {/* <a href="#!">Contact</a>
+          <a href="#!">Privacy Policy</a> */}
+          <a href="#!">
+            <FaFacebookF />
+          </a>
+          <a href="#!">
+            <FaTwitter />
+          </a>
+          <a href="#!">
+            <FaInstagram />
+          </a>
         </div>
       </div>
       <div className="copyRightSection">
