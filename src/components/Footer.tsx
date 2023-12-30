92faf9f9774bb6d6beda497f46bd95fcf89f1b0f
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-12-30 14:10:44 -0400

    Home complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React from "react";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa"; // Import the icons
 
 const Footer: React.FC = () => {
+  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
   return (
     <footer className="footer">
       <div className="footerContent">
-        <div className="linkSection">
+        <div className="mapSection">
+          <iframe
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            loading="lazy"
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+            src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=9.845323,-83.940137`}
+          ></iframe>
+        </div>
+        {/* <div className="linkSection">
+        </div> */}
+        <div className="socialMediaSection">
           <a href="#!">About Us</a>
           <a href="#!">Contact</a>
           <a href="#!">Privacy Policy</a>
-        </div>
-        <div className="socialMediaSection">
-          <a href="#!">Facebook</a>
-          <a href="#!">Twitter</a>
-          <a href="#!">Instagram</a>
+          <a href="#!"><FaFacebookF /></a>
+          <a href="#!"><FaTwitter /></a>
+          <a href="#!"><FaInstagram /></a>
         </div>
       </div>
       <div className="copyRightSection">
-        © {new Date().getFullYear()} Your Website Name
+        © {new Date().getFullYear()} Super Fe
       </div>
     </footer>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head'; // Import the Head component
 import Layout from '@/app/layout';
 import '../styles/global.scss';
 
-
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500&display=swap" rel="stylesheet" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
