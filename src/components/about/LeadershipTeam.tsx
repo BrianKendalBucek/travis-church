46e320043bb176876135bdfc80962adadecb990d
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2024-03-02 12:55:37 -0600

    improved about media queries for leadership

--- src/components/about/LeadershipTeam.tsx
+++ src/components/about/LeadershipTeam.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import Image from "next/image";
-import styles from '../../styles/LeadershipTeam.module.scss';
 
 export default function LeadershipTeam() {
   return (
-    <section className={styles.leadershipTeam}>
-      <h2 className={styles.aboutSpace}>Equipo de Liderazgo</h2>
-      <p className={styles.briefDesc}>
+    <section className="leadership-team">
+      <h2 className="about-space">Equipo de Liderazgo</h2>
+      <p className="brief-desc">
         Conoce a las personas dedicadas que lideran la Iglesia de Travis y
         Paulina con pasión y fe. Nuestro equipo de liderazgo está comprometido
         en guiar a nuestra congregación y servir a nuestra comunidad.
       </p>
-      <div className={styles.leadershipList}>
-        <div className={styles.leaderProfile}>
-          <Image src="/don.jpg" alt="Leader 1" width={300} height={300} />
+      <div className="leadership-list">
+        <div className="leader-profile">
+          <Image className="leader-profile-img" src="/don.jpg" alt="Leader 1" width={300} height={300} />
           <h3>Pastor Travis Reid</h3>
         </div>
-        <div className={styles.leaderProfile}>
-          <Image src="/betty.jpg" alt="Leader 2" width={300} height={300} />
+        <div className="leader-profile">
+          <Image className="leader-profile-img" src="/betty.jpg" alt="Leader 2" width={300} height={300} />
           <h3>Pastora Paulina Reid</h3>
         </div>
       </div>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -19,7 +19,7 @@ export default function AboutUs() {
       <div className="baptism"></div>
       <CommunityInvolvement />
       <div className="community"></div>
-      {/* <LeadershipTeam /> */}
+      <LeadershipTeam />
       {/* </div> */}
     </div>
   );
